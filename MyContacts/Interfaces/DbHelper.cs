aedf88f06aab6812ae58f5889a060210f32eee70
amin karvizi <aminkarvizi1384@gmail.com>
2020-11-17 14:53:44 +0330

    updating DbHelper

--- MyContacts/Interfaces/DbHelper.cs
+++ MyContacts/Interfaces/DbHelper.cs
@@ -30,17 +30,20 @@ namespace MyContacts.Interfaces
         //return login form inputs errors where LoginFormValidation() returned false 
         public abstract string LoginFormErrors(string username, string pwd, string confirmpwd);
 
-
-        //if all thing was valid the ShowContacts form must Show()
+        //if all things were valid the ShowContacts form must Show()
         public abstract void Login();
 
         //if all thing was valid the Login form must Show() after the user inserted
         public abstract void Register();
 
+        //check are newcontact forms inputs valid
+        public abstract bool IsValidContact(string name, string ip, int Age);
 
-        public abstract void InsertContact(string name, string ip, int Age);
-        public abstract bool IsValidContact(string name , string ip , int Age);
+        //return login form inputs errors where LoginFormValidation() returned false 
         public abstract string ValidationContactError(string name, string ip, int Age);
+
+        //if all things were valid the Contacts form must Show() after the contact inserted
+        public abstract void InsertContact(string name, string ip, int Age);
 
     }
 }
