0a23d28fc06b7b38c2968f171b6b7300fb054872
amin karvizi <aminkarvizi1384@gmail.com>
2020-11-18 21:21:40 +0330

    R updated

--- MyContacts/R.cs
+++ MyContacts/R.cs
@@ -6,6 +6,19 @@
 		{
 			// put your placeholders here
 			// e.g : public const string age_placeholder = "سن";
+
+			public const string age_placeholder = "سن";
+
+			public const string username_placeholder = "نام کاربری";
+
+			public const string password_placeholder = "کلمه ی عبور";
+
+			public const string confirmpassword_placeholder = "تکرار کلمه ی عبور";
+
+			public const string ip_placeholder = "آی پی";
+
+			public const string fullname_placeholder = "نام و نام خانوادگی";
+
 		}
 	}
 }
